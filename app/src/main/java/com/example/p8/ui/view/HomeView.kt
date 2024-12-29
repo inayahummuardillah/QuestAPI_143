5336b4f02eebad0358b30f4c0e2d8d49a3722d4c
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2024-12-30 03:47:06 +0630

    Membuat OnLoading dan OnError untuk menampilkan data yang sedang dimuat atau terjai kesalahan

--- app/src/main/java/com/example/p8/ui/view/HomeView.kt
+++ app/src/main/java/com/example/p8/ui/view/HomeView.kt
@@ -135,4 +135,36 @@ fun HomeStatus(
         )
     }
 }
+@Composable
+fun OnLoading (modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier:
+    Modifier = Modifier
+) {
+    Column(
+        modifier =
+        modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.
+        padding(16.dp))
+        Button(onClick =
+        retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
 
